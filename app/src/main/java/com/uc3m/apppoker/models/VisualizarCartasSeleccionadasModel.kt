dfced72676c1d57b878495cfc21354b275b4b7e3
corsa <rubi5914@gmail.com>
2021-04-26 15:20:39 +0200

    cgnat interfaz 1.0

--- app/src/main/java/com/uc3m/apppoker/models/VisualizarCartasSeleccionadasModel.kt
+++ app/src/main/java/com/uc3m/apppoker/models/VisualizarCartasSeleccionadasModel.kt
@@ -3,6 +3,10 @@ package com.uc3m.apppoker.models
 import android.content.Context
 import android.util.Log
 import android.view.View
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -15,6 +19,7 @@ import androidx.navigation.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.uc3m.apppoker.views.VisualizarCartasSeleccionadasFragment
 import com.wandroid.traceroute.TraceRoute
 
 class VisualizarCartasSeleccionadasModel {
@@ -31,10 +36,21 @@ class VisualizarCartasSeleccionadasModel {
 
                 Log.d("Response -------->>>>", ip)
 
+                val botonStart: Button = v.findViewById(R.id.botonSubmit)
+                val infoInicial: TextView = v.findViewById(R.id.textoInicial)
+                var animacion : Animation = AlphaAnimation(1.0f,0.0f)
+                animacion.duration = 5000
+                botonStart.startAnimation(animacion)
+                botonStart.visibility = View.GONE
+                infoInicial.startAnimation(animacion)
+                infoInicial.visibility = View.GONE
+
                 TraceRoute.setCallback {
                     success {
                         Log.d("tagg", VariablesGlobales.resultadoTraceroute)
-                        //mostrarTraceroute()
+                       val viewVisualizarResultado = VisualizarCartasSeleccionadasFragment ()
+                        viewVisualizarResultado.mostrarTraceroute(v)
+
                         almacenarResultadoBaseDatos(VariablesGlobales.resultadoTraceroute)
 
                     }
@@ -66,22 +82,11 @@ class VisualizarCartasSeleccionadasModel {
             .child(FirebaseAuth.getInstance().currentUser.uid)
             .child("Datos recopilados")
             .child("traceroute").setValue(resultado)
-       /* database.child("users")
-                .child(FirebaseAuth.getInstance().currentUser.uid)
-                .child("estadisticas")
-                .child(resultadoTrad).get().addOnSuccessListener {
+        database.child("users")
+            .child(FirebaseAuth.getInstance().currentUser.uid)
+            .child("Usuario")
+            .setValue(FirebaseAuth.getInstance().currentUser.displayName)
 
-                    var contador : Long = it.value.toString().toLong()
-                    //EncryptModel.decryptData(iv,it.value as ByteArray) as Long
-                    contador = contador + 1
-                    database.child("users")
-                            .child(FirebaseAuth.getInstance().currentUser.uid)
-                            .child("estadisticas")
-                            .child(resultadoTrad).setValue(contador)
-
-                }.addOnFailureListener{
-                    Log.e("firebase", "Error getting data", it)
-                }*/
     }
 
     fun comprobarUsuarioEnBaseDatos (){
@@ -106,76 +111,6 @@ class VisualizarCartasSeleccionadasModel {
 
     }
 
-    fun comprobarCartas(mesa: Array<String?>, jugadores: Array<String?>, context: Context): Boolean{
-
-        var contieneNull1 = false
-        var contieneNull2 = false
-        var finalPareja = false
-        for( i in mesa){
-            if (i == null){
-                Toast.makeText(context, "Faltan cartas en la mesa", Toast.LENGTH_SHORT).show()
-                return false
-            }
-        }
-        var todosNull = true
-        for( i in jugadores){
-            if (i != null){
-
-                todosNull = false
-            }
-        }
-        if (todosNull){
-            Toast.makeText(context, "Añadir al menos un jugador", Toast.LENGTH_SHORT).show()
-            return false
-        }
-        for ((n,i) in jugadores.withIndex()){
-            if (n % 2 != 0){
-                finalPareja = true
-            }
-            if(!finalPareja){
-                contieneNull1 = ( i == null)
-            }else {
-                contieneNull2 = (i == null)
-            }
-            finalPareja = false
 
 
-            if (n % 2 != 0){
-                if (contieneNull1 != contieneNull2){
-                    Toast.makeText(context, "Jugador mal introducido", Toast.LENGTH_SHORT)
-                            .show()
-                    return false
-                }
-            }
-
-        }
-
-
-        val cartas = arrayOfNulls<String>(mesa.size + jugadores.size)
-        for ((n,i) in mesa.withIndex()){
-            cartas[n] = i
-        }
-        var k = mesa.size
-        for ((n,i) in jugadores.withIndex()){
-            k += n
-            cartas[k] = i
-            k -= n
-        }
-
-        for((n, i) in cartas.withIndex()){
-            for ((k, j) in cartas.withIndex()){
-                if (n != k ){
-                    if (i == j && i != null ){
-                        Toast.makeText(context,
-                                "No puede haber cartas repetidas", Toast.LENGTH_SHORT).show()
-                        return false
-                    }
-                }
-
-            }
-        }
-
-
-        return true
-    }
 }
--- app/src/main/java/com/uc3m/apppoker/views/VisualizarCartasSeleccionadasFragment.kt
+++ app/src/main/java/com/uc3m/apppoker/views/VisualizarCartasSeleccionadasFragment.kt
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
 import android.widget.*
 import androidx.activity.addCallback
 import androidx.lifecycle.ViewModelProvider
@@ -59,9 +61,6 @@ class VisualizarCartasSeleccionadasFragment : Fragment() {
         val view = binding.root
 
         modelVisualizarCartasSeleccionadas.comprobarUsuarioEnBaseDatos()
-       // val traceresult = TraceRoute.traceRoute("www.google.com")
-        //Log.d("eee",traceresult?.message.toString())
-
 
 
         binding.BotonLogOutt.setOnClickListener{
@@ -88,171 +87,7 @@ class VisualizarCartasSeleccionadasFragment : Fragment() {
         }
 
 
-        binding.anadirCarta1.setOnClickListener {
-            findNavController().navigate(
-                    R.id.action_visualizarCartasSeleccionadasFragment_to_elegirPaloFragment)
-            VariablesGlobales.posicionCarta = 1
 
-
-        }
-        binding.anadirCarta2.setOnClickListener {
-            findNavController().navigate(
-                    R.id.action_visualizarCartasSeleccionadasFragment_to_elegirPaloFragment)
-            VariablesGlobales.posicionCarta = 2
-
-
-        }
-        binding.anadirCarta3.setOnClickListener {
-            findNavController().navigate(
-                    R.id.action_visualizarCartasSeleccionadasFragment_to_elegirPaloFragment)
-            VariablesGlobales.posicionCarta = 3
-
-
-        }
-        binding.anadirCarta4.setOnClickListener {
-            findNavController().navigate(
-                    R.id.action_visualizarCartasSeleccionadasFragment_to_elegirPaloFragment)
-            VariablesGlobales.posicionCarta = 4
-
-
-        }
-        binding.anadirCarta5.setOnClickListener {
-            findNavController().navigate(
-                    R.id.action_visualizarCartasSeleccionadasFragment_to_elegirPaloFragment)
-            VariablesGlobales.posicionCarta = 5
-
-
-        }
-        binding.anadirCarta6.setOnClickListener {
-            findNavController().navigate(
-                    R.id.action_visualizarCartasSeleccionadasFragment_to_elegirPaloFragment)
-            VariablesGlobales.posicionCarta = 6
-
-
-        }
-        binding.anadirCarta7.setOnClickListener {
-            findNavController().navigate(
-                    R.id.action_visualizarCartasSeleccionadasFragment_to_elegirPaloFragment)
-            VariablesGlobales.posicionCarta = 7
-
-
-        }
-
-        binding.anadirCarta8.setOnClickListener {
-            findNavController().navigate(
-                    R.id.action_visualizarCartasSeleccionadasFragment_to_elegirPaloFragment)
-            VariablesGlobales.posicionCarta = 8
-
-
-        }
-        binding.anadirCarta9.setOnClickListener {
-            findNavController().navigate(
-                    R.id.action_visualizarCartasSeleccionadasFragment_to_elegirPaloFragment)
-            VariablesGlobales.posicionCarta = 9
-
-
-        }
-        binding.anadirCarta10.setOnClickListener {
-            findNavController().navigate(
-                    R.id.action_visualizarCartasSeleccionadasFragment_to_elegirPaloFragment)
-            VariablesGlobales.posicionCarta = 10
-
-
-        }
-        binding.anadirCarta11.setOnClickListener {
-            findNavController().navigate(
-                    R.id.action_visualizarCartasSeleccionadasFragment_to_elegirPaloFragment)
-            VariablesGlobales.posicionCarta = 11
-
-
-        }
-        binding.anadirCarta12.setOnClickListener {
-            findNavController().navigate(
-                    R.id.action_visualizarCartasSeleccionadasFragment_to_elegirPaloFragment)
-            VariablesGlobales.posicionCarta = 12
-
-
-        }
-        binding.anadirCarta13.setOnClickListener {
-            findNavController().navigate(
-                    R.id.action_visualizarCartasSeleccionadasFragment_to_elegirPaloFragment)
-            VariablesGlobales.posicionCarta = 13
-
-
-        }
-        binding.anadirCarta14.setOnClickListener {
-            findNavController().navigate(
-                    R.id.action_visualizarCartasSeleccionadasFragment_to_elegirPaloFragment)
-            VariablesGlobales.posicionCarta = 14
-
-
-        }
-        binding.anadirCarta15.setOnClickListener {
-            findNavController().navigate(
-                    R.id.action_visualizarCartasSeleccionadasFragment_to_elegirPaloFragment)
-            VariablesGlobales.posicionCarta = 15
-
-
-        }
-        binding.anadirCarta16.setOnClickListener {
-            findNavController().navigate(
-                    R.id.action_visualizarCartasSeleccionadasFragment_to_elegirPaloFragment)
-            VariablesGlobales.posicionCarta = 16
-
-
-        }
-        binding.anadirCarta17.setOnClickListener {
-            findNavController().navigate(R
-                    .id.action_visualizarCartasSeleccionadasFragment_to_elegirPaloFragment)
-            VariablesGlobales.posicionCarta = 17
-
-
-        }
-        binding.eliminar1.setOnClickListener{
-            VariablesGlobales.seleccionTotalCartasJugadores[0] = null
-            VariablesGlobales.seleccionTotalCartasJugadores[1] = null
-            view.findViewById<ImageButton>(R.id.anadirCarta6)
-                    .setBackgroundResource(R.drawable.addcard)
-            view.findViewById<ImageButton>(R.id.anadirCarta7)
-                    .setBackgroundResource(R.drawable.addcard)
-        }
-        binding.eliminar2.setOnClickListener{
-            VariablesGlobales.seleccionTotalCartasJugadores[2] = null
-            VariablesGlobales.seleccionTotalCartasJugadores[3] = null
-            view.findViewById<ImageButton>(R.id.anadirCarta8)
-                    .setBackgroundResource(R.drawable.addcard)
-            view.findViewById<ImageButton>(R.id.anadirCarta9)
-                    .setBackgroundResource(R.drawable.addcard)
-        }
-        binding.eliminar3.setOnClickListener{
-            VariablesGlobales.seleccionTotalCartasJugadores[4] = null
-            VariablesGlobales.seleccionTotalCartasJugadores[5] = null
-            view.findViewById<ImageButton>(R.id.anadirCarta10)
-                    .setBackgroundResource(R.drawable.addcard)
-            view.findViewById<ImageButton>(R.id.anadirCarta11)
-                    .setBackgroundResource(R.drawable.addcard)
-        }
-        binding.eliminar4.setOnClickListener{
-            VariablesGlobales.seleccionTotalCartasJugadores[6] = null
-            VariablesGlobales.seleccionTotalCartasJugadores[7] = null
-            view.findViewById<ImageButton>(R.id.anadirCarta12)
-                    .setBackgroundResource(R.drawable.addcard)
-            view.findViewById<ImageButton>(R.id.anadirCarta13)
-                    .setBackgroundResource(R.drawable.addcard)
-        }
-        binding.eliminar5.setOnClickListener{
-            VariablesGlobales.seleccionTotalCartasJugadores[8] = null
-            VariablesGlobales.seleccionTotalCartasJugadores[9] = null
-            view.findViewById<ImageButton>(R.id.anadirCarta14)
-                    .setBackgroundResource(R.drawable.addcard)
-            view.findViewById<ImageButton>(R.id.anadirCarta15)
-                    .setBackgroundResource(R.drawable.addcard)
-        }
-
-        binding.botonestadisticas.setOnClickListener{
-            findNavController()
-                    .navigate(R.id.action_visualizarCartasSeleccionadasFragment_to_estadisticas)
-        }
         binding.botonSubmit.setOnClickListener{
 
           /*  val datosCorrectos = context?.let { it1 ->
@@ -274,83 +109,29 @@ class VisualizarCartasSeleccionadasFragment : Fragment() {
 
         return view
     }
+    fun mostrarTraceroute(v : View){
+
+        val presentardatos : TextView = v.findViewById(R.id.textoresultado)
+        val mostrardatos: TextView = v.findViewById(R.id.textomostrarresultado)
 
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+        mostrardatos.text = VariablesGlobales.resultadoTraceroute
 
-        colocarCarta()
+        var animacion : Animation = AlphaAnimation(0.0f,1.0f)
+        animacion.duration = 5000
+        presentardatos.visibility = View.VISIBLE
+        presentardatos.startAnimation(animacion)
+        mostrardatos.visibility = View.VISIBLE
+        mostrardatos.startAnimation(animacion)
+
+
     }
 
 
-    private fun colocarCarta() {
-
-        if (VariablesGlobales.posicionCarta != 0) {
-            if (VariablesGlobales.posicionCarta != 100) {
 
 
-                if (VariablesGlobales.posicionCarta < 6) {
-
-                    VariablesGlobales
-                            .seleccionTotalCartasMesa[VariablesGlobales.posicionCarta - 1] =
-                        cartaSeleccionada
-
-                } else {
-
-                    VariablesGlobales
-                            .seleccionTotalCartasJugadores[VariablesGlobales.posicionCarta - 6] =
-                        cartaSeleccionada
-
-                }
-            }
-                var n = 1
-                for (i in VariablesGlobales.seleccionTotalCartasMesa) {
-
-                    if (i != null) {
-
-                        view?.findViewById<ImageButton>(
-                            getResources().getIdentifier(
-                                    "anadirCarta$n",
-                                "id",
-                                context?.packageName
-                            )
-                        )?.setBackgroundResource(
-                            getResources().getIdentifier(
-                                i,
-                                "drawable",
-                                context?.packageName
-                            )
-                        )
-                    }
-                    n++
-                }
-                var o = 6
-
-                for (i in VariablesGlobales.seleccionTotalCartasJugadores) {
-
-                    if (i != null) {
 
 
-                        view?.findViewById<ImageButton>(
-                            resources.getIdentifier(
-                                    "anadirCarta$o",
-                                "id",
-                                context?.packageName
-                            )
-                        )?.setBackgroundResource(
-                            resources.getIdentifier(
-                                i,
-                                "drawable",
-                                context?.packageName
-                            )
-                        )
-
-                    }
-                    o++
-                }
-                VariablesGlobales.posicionCarta = 0
-        }
-    }
 }
 
 
